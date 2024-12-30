277ffecda7ab269ddf4cfd582f15df63d7c133ae
sujitha769 <163429668+sujitha769@users.noreply.github.com>
2024-12-30 16:07:27 +0530

    Add files via upload

---
+++ 3calcExport.js
@@ -0,0 +1,8 @@
+const addition=(a,b)=>a+b;
+const subtraction=(a,b)=>a-b;
+const multiplication=(a,b)=>a*b;
+const division=(a,b)=>a/b;
+const remainder=(a,b)=>a%b;
+
+module.exports={addition,subtraction,multiplication,division,remainder};
+
