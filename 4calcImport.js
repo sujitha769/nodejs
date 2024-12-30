cecd4666f36681105a93e51211620a8a18ec8bc9
sujitha769 <163429668+sujitha769@users.noreply.github.com>
2024-12-30 16:07:50 +0530

    Add files via upload

---
+++ 4calcImport.js
@@ -0,0 +1,6 @@
+const name=require('./3calcExport')
+console.log(name.addition(4,5));
+console.log(name.subtraction(4,5));
+console.log(name.multiplication(4,5));
+console.log(name.division(4,5));
+console.log(name.remainder(4,5));
